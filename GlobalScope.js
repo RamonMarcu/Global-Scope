9c71be0dc36d0e39bda5b38f8acbf97ff38ad19e
Ramon Marcu <ramondotmarcu@gmail.com>
2021-06-23 17:51:43 +0300

    Add files via upload

---
+++ GlobalScope.js
@@ -0,0 +1,13 @@
+//main.js
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+const callMyNightSky = () => {
+ return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+};
+
+console.log(callMyNightSky());
+
+// Night Sky: The Moon, North Star, and The Milky Way
